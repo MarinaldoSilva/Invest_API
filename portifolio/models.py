7947c484405fe758aef3694581598e9be7a45c9b
Mario <marinaldo12@hotmail.com>
2025-11-28 10:37:15 -0300

    alteração da pk para uuid

--- portifolio/models.py
+++ portifolio/models.py
@@ -1,11 +1,10 @@
-import uuid as uuid_
-
+from uuid import uuid4
 from django.conf import settings
 from django.db import models
 
 
 class Portifolio(models.Model):
-    uuid = models.UUIDField(default=uuid_.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome_carteira = models.CharField(max_length=255, null=False, blank=False)
     dono = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, name="portifolio")
     
